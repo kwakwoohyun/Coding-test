7091597b88d3d348e63cef1f433ea6707812f2a8
unknown <kwakwoohyun010@gmail.com>
2020-09-01 22:33:38 +0900

    backjoon first step

---
+++ 입출력과사칙연산/backjoon10171.java
@@ -0,0 +1,10 @@
+package 입출력과사칙연산;
+
+public class backjoon10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	}
+
+}
---
+++ 입출력과사칙연산/backjoon10172.java
@@ -0,0 +1,8 @@
+package 입출력과사칙연산;
+
+public class backjoon10172 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+	}
+}
