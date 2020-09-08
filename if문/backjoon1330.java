a9748fcdd975e34b1462b7c104c5dd2512143925
unknown <kwakwoohyun010@gmail.com>
2020-09-08 23:15:01 +0900

    backjoon second step

---
+++ if문/backjoon1330.java
@@ -0,0 +1,21 @@
+package if문;
+
+import java.util.Scanner;
+
+public class backjoon1330 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		if (A > B) {
+			System.out.println(">");
+		} else if (A < B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+
+}
---
+++ if문/backjoon9498.java
@@ -0,0 +1,24 @@
+package if문;
+
+import java.util.Scanner;
+
+public class backjoon9498 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int grade = sc.nextInt();
+		if (grade >= 90 && grade <= 100) {
+			System.out.println('A');
+		} else if (grade >= 80 && grade <= 89) {
+			System.out.println('B');
+		} else if (grade >= 70 && grade <= 79) {
+			System.out.println('C');
+		} else if (grade >= 60 && grade <= 69) {
+			System.out.println('D');
+		} else {
+			System.out.println('F');
+		}
+	}
+
+}
